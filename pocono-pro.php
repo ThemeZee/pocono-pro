d5d2ee6d19919c9f5e5ebeb6fc298bade2bc216b
Lenovo-PC\thomasw <kontakt@themecoder.de>
2017-01-31 15:27:21 +0100

    updated changelog and version number

--- pocono-pro.php
+++ pocono-pro.php
@@ -5,7 +5,7 @@ Plugin URI: http://themezee.com/addons/pocono-pro/
 Description: Adds additional features like custom colors, google fonts, widget areas and footer copyright to the Pocono theme.
 Author: ThemeZee
 Author URI: https://themezee.com/
-Version: 1.0
+Version: 1.0.1
 Text Domain: pocono-pro
 Domain Path: /languages/
 License: GPL v3
@@ -62,7 +62,7 @@ class Pocono_Pro {
 		define( 'POCONO_PRO_NAME', 'Pocono Pro' );
 
 		// Define Version Number.
-		define( 'POCONO_PRO_VERSION', '1.0' );
+		define( 'POCONO_PRO_VERSION', '1.0.1' );
 
 		// Define Plugin Name.
 		define( 'POCONO_PRO_PRODUCT_ID', 89730 );
